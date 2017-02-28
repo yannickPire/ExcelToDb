1b421974eb810c653235394d4f4585ad84998571
Yannick <Yannick@Yannick-Laptop>
2017-02-28 21:13:21 +0100

    Add the location through a parammeter and also default location

--- src/main/java/com/axa/SpringBootToMySqlApplication.java
+++ src/main/java/com/axa/SpringBootToMySqlApplication.java
@@ -1,11 +1,11 @@
 package com.axa;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,24 +16,27 @@ import com.axa.models.User;
 import com.axa.repo.UserRepository;
 
 @SpringBootApplication
-public class SpringBootToMySqlApplication {
+public class SpringBootToMySqlApplication implements CommandLineRunner {
+
+	private String location;
+	
+	@Value("${name:unknown}")
+	private String name;
 
 	private static final Logger log = LoggerFactory.getLogger(SpringBootApplication.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(SpringBootToMySqlApplication.class, args);
 	}
 
 	@Bean
 	public CommandLineRunner demo(UserRepository repository) {
 		return (args) -> {
-			
 
-			List<User> users = ExcelReader.readExcel("C:\\Users\\Yannick.Pire\\Desktop\\test.xls");
-			for(User user : users){
+			List<User> users = ExcelReader.readExcel(getLocation());
+			for (User user : users) {
 				repository.save(user);
 			}
-			
 
 			// fetch all customers
 			log.info("Customers found with findAll():");
@@ -43,5 +46,23 @@ public class SpringBootToMySqlApplication {
 			}
 			log.info("");
 		};
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		if (args.length > 0) {
+			setLocation(args[0]);
+		} else {
+			setLocation(name);
+		}
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
 	}
 }
